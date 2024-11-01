9632fd6c54dc3b90be2e8f0936a93341fa3c1d76
Dmitriy Agarkov <Dmitrii.agarkov@gmail.com>
2024-11-01 18:38:16 +0200

    add package json, index.js, .gitignore

---
+++ nodeBasics/index.js
@@ -0,0 +1,9 @@
+import chalk from "chalk";
+
+const firstName = 'Daniel';
+const lasName = 'Miller';
+let age = 35;
+
+console.log(chalk.bgGreen(firstName));
+console.log(chalk.bgBlack(lasName));
+console.log(chalk.bgBlue(age))
